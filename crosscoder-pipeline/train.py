b3079c941d12a0f6542b4e4bce586134b5359e63
Kenny <keneaton242@gmail.com>
2025-02-15 15:52:30 -0500

    Cleaned code with isort and flake8

--- crosscoder-pipeline/train.py
+++ crosscoder-pipeline/train.py
@@ -1,19 +1,78 @@
-from utils import *
+import argparse
+import json
+
+import torch
+import yaml
+from datasets import load_dataset
+from huggingface_hub import login
+from transformer_lens import HookedTransformer
+
 from trainer import Trainer
 
 
-with open('config.yaml', 'r') as file:
+def arg_parse_update_cfg(default_cfg):
+    """
+    Helper function to take in a dictionary of arguments,
+    convert these to command line arguments,
+    look at what was passed in, and return an updated dictionary.
+    If in Ipython, just returns with no changes
+    """
+    cfg = dict(default_cfg)
+    parser = argparse.ArgumentParser()
+    for key, value in default_cfg.items():
+        if type(value) is bool:
+            # argparse for Booleans is broken rip.
+            # Now you put in a flag to change the default --{flag} to set True,
+            # --{flag} to set False
+            if value:
+                parser.add_argument(f"--{key}", action="store_false")
+            else:
+                parser.add_argument(f"--{key}", action="store_true")
+        else:
+            parser.add_argument(f"--{key}", type=type(value), default=value)
+    args = parser.parse_args()
+    parsed_args = vars(args)
+    cfg.update(parsed_args)
+    print("Updated config")
+    print(json.dumps(cfg, indent=2))
+    return cfg
+
+
+def load_pile_lmsys_mixed_tokens():
+    try:
+        print("Loading data from disk")
+        all_tokens = torch.load(
+            "workspace/data/pile-lmsys-mix-1m-tokenized-gemma-2.pt")
+    except Exception:
+        print("Data is not cached. Loading data from HF")
+        data = load_dataset(
+            "ckkissane/pile-lmsys-mix-1m-tokenized-gemma-2",
+            split="train",
+            cache_dir="workspace/cache/",
+        )
+        data.save_to_disk(
+            "workspace/data/pile-lmsys-mix-1m-tokenized-gemma-2.hf")
+        data.set_format(type="torch", columns=["input_ids"])
+        all_tokens = data["input_ids"]
+        torch.save(all_tokens,
+                   "workspace/data/pile-lmsys-mix-1m-tokenized-gemma-2.pt")
+        print("Saved tokens to disk")
+    return all_tokens
+
+
+with open("config.yaml", "r") as file:
     default_cfg = yaml.safe_load(file)
-    
+
+login(token=default_cfg["hf_token"])
 base_model = HookedTransformer.from_pretrained(
-    "gemma-2-2b", 
-    device=default_cfg['device'], 
+    "gemma-2-2b",
+    device=default_cfg["device"],
 )
 chat_model = HookedTransformer.from_pretrained(
-    "gemma-2-2b-it", 
-    device=default_cfg['device'], 
+    "gemma-2-2b-it",
+    device=default_cfg["device"],
 )
-default_cfg['d_in'] = base_model.cfg.d_model
+default_cfg["d_in"] = base_model.cfg.d_model
 all_tokens = load_pile_lmsys_mixed_tokens()
 cfg = arg_parse_update_cfg(default_cfg)
 trainer = Trainer(cfg, base_model, chat_model, all_tokens)
--- crosscoder-pipeline/trainer.py
+++ crosscoder-pipeline/trainer.py
@@ -1,9 +1,12 @@
-from utils import *
-from crosscoder import CrossCoder
-from buffer import Buffer
+import torch
 import tqdm
-
 from torch.nn.utils import clip_grad_norm_
+
+import wandb
+from buffer import Buffer
+from crosscoder import CrossCoder
+
+
 class Trainer:
     def __init__(self, cfg, model_A, model_B, all_tokens):
         self.cfg = cfg
@@ -12,7 +15,6 @@ class Trainer:
         self.crosscoder = CrossCoder(cfg)
         self.buffer = Buffer(cfg, model_A, model_B, all_tokens)
         self.total_steps = cfg["num_tokens"] // cfg["batch_size"]
-
         self.optimizer = torch.optim.Adam(
             self.crosscoder.parameters(),
             lr=cfg["lr"],
@@ -22,19 +24,21 @@ class Trainer:
             self.optimizer, self.lr_lambda
         )
         self.step_counter = 0
-
         wandb.init(project=cfg["wandb_project"], entity=cfg["wandb_entity"])
 
     def lr_lambda(self, step):
         if step < 0.8 * self.total_steps:
             return 1.0
         else:
-            return 1.0 - (step - 0.8 * self.total_steps) / (0.2 * self.total_steps)
+            return 1.0 - (step - 0.8 * self.total_steps) / (0.2
+                                                            * self.total_steps)
 
     def get_l1_coeff(self):
-        # Linearly increases from 0 to cfg["l1_coeff"] over the first 0.05 * self.total_steps steps, then keeps it constant
+        # Linearly increases from 0 to cfg["l1_coeff"]
+        # over the first 0.05 * self.total_steps steps, then keeps it constant
         if self.step_counter < 0.05 * self.total_steps:
-            return self.cfg["l1_coeff"] * self.step_counter / (0.05 * self.total_steps)
+            return self.cfg["l1_coeff"] * self.step_counter \
+                / (0.05 * self.total_steps)
         else:
             return self.cfg["l1_coeff"]
 
@@ -47,7 +51,7 @@ class Trainer:
         self.optimizer.step()
         self.scheduler.step()
         self.optimizer.zero_grad()
-
+        self.step_counter += 1
         loss_dict = {
             "loss": loss.item(),
             "l2_loss": losses.l2_loss.item(),
@@ -59,7 +63,6 @@ class Trainer:
             "explained_variance_A": losses.explained_variance_A.mean().item(),
             "explained_variance_B": losses.explained_variance_B.mean().item(),
         }
-        self.step_counter += 1
         return loss_dict
 
     def log(self, loss_dict):
